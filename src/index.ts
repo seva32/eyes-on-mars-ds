d9160088145581cc20caff5a80b0de604fbcfebe
Sebs <seva32@yahoo.es>
2025-03-24 18:09:14 -0400

    adds pagination component

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from "./Button/Button";
 export { default as Dropzone } from "./Dropzone/Dropzone";
 export { default as Input } from "./Input/Input";
+export { default as Pagination } from "./Pagination/Pagination";
 export { default as Toast } from "./Toast/Toast";
